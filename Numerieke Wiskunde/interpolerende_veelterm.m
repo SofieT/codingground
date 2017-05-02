e3c6e6a3b653b727956be8511a885daaa9db0a3d
SofieT <super_sofie_smpp@hotmail.com>
2017-05-02 20:51:32 +0200

    Update interpolerende_veelterm.m

--- Numerieke Wiskunde/interpolerende_veelterm.m
+++ Numerieke Wiskunde/interpolerende_veelterm.m
@@ -1,16 +1,24 @@
 function p interpolerende_veelterm(x, fun, type)
    coef = 1:1:length(x);
    
+   v = 1:1:length(x);
+   for i = 1:length(x)
+         v(i) = feval(fun, x(i));
+   end
+   
    switch type
         case "monomiaal"
-            coef = coefMonomiaal(x, fun);            
+            A = evalbasis(x, length(x), @monomiaalbasis);            
         case "lagrange"
-            coef = coefLagrange(x, fun);
+            A = evalbasis(x, length(x), @langrangebasis, x);
         case "newton"
-            coef = coefNewton(x, fun);
+            A = evalbasis(x, length(x), @newtonbasis, x);
         case "chebyshev"
-            coef = coefChebyshev(x, fun);
+            A = evalbasis(x, length(x), @chebyshevbasis);
    end 
+   
+   coef = A\v;
+   val = @evalueer;
    p = struct(coef, val);
    
 end
